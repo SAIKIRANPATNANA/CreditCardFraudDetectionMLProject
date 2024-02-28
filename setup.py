b81a4d2201224426568bdb2f28e117b5f5ba57ea
SAIKIRANPATNANA <saikiranpatnana5143@gmail.com>
2024-02-28 17:21:07 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,40 +1,22 @@
-from setuptools import setup,find_packages
+from setuptools import find_packages,setup
 from typing import List
-def get_requirements(file_path:str)-> List[str]:
-    editable = '-e .'
-    with open(file_path,'r') as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace('\n','') for req in requirements if editable not in req]
-        return requirements
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
 setup(
-    name = 'CreditCardFaultDetecton',
+    name="CreditCardFraudDetectionMLProject",
     version='0.0.1',
-    author='SaiKiranPatnana',
-    author_email= 'saikiranpatnana5143@gmail.com',
-    install_requires = get_requirements('/config/workspace/requirements.txt'),
-    packages = find_packages()
+    author='SAIKIRANPATNANA',
+    author_email='saikiranpatnana5143@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
 )
-# from setuptools import find_packages,setup
-# from typing import List
-# HYPHEN_E_DOT = '-e .'
-# def get_requirements(file_path:str)->List[str]:
-#     requirements = []
-#     with open(file_path) as file_obj:
-#         requirements = file_obj.readlines()
-#         requirements = [req.replace("\n","") for req in requirements]
-#         if HYPHEN_E_DOT in requirements:
-#                 requirements.remove(HYPHEN_E_DOT)
-#         return requirements
-
-# setup (
-#     name = 'DiamondPricePrediction',
-#     version = '0.0.1',
-#     author = 'SaiKiranPatnana',
-#     author_email ='saikiranpatnana5143@gmail.com',
-#     install_requires = get_requirements('requirements.txt'),
-#     packages = find_packages()
-# )
-
-    
-
-
